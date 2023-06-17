46b99719cf5c4b94ccb596214dd9d84ec115a7a9
Álef de Souza Rosa <alefsouzamac1023@gmail.com>
2023-06-17 16:16:15 -0300

    introduction-page

---
+++ src/pages/Carrossel.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-carousel v-model="step" hide-delimiters>
+        <v-carousel-item v-for="(item, index) in items" :key="index" :src="item.backgroundImage">
+            <v-container fill-height>
+                <v-layout column align-center justify-center>
+                    <v-img :src="logo" max-height="80"></v-img>
+                </v-layout>
+                <v-card class="card" outlined>
+                    <v-card-title>{{ item.title }}</v-card-title>
+                    <v-card-text>{{ item.text }}</v-card-text>
+                    <v-layout justify-center align-center>
+                        <v-btn v-if="step > 0" @click="$router.push('/login')">Saltar</v-btn>
+                        <v-carousel-indicators :value="step" :length="items.length"></v-carousel-indicators>
+                        <v-btn @click="nextStep" v-if="step < items.length - 1">
+                            Seguinte
+                        </v-btn>
+                    </v-layout>
+                </v-card>
+            </v-container>
+        </v-carousel-item>
+    </v-carousel>
+</template>
+  
+<script>
+export default {
+    name: "carrossel",
+    data() {
+        return {
+            step: 0,
+            items: [
+                {
+                    title: 'Chegou um momento de mudança para o mundo!',
+                    text: 'Explora toda a informação sobre o evento THE CHANGE - a mudança chegou!',
+                    backgroundImage: '../assets/step1Background.png'
+                },
+                {
+                    title: 'Faz parte de um evento transformador!',
+                    text: 'Vamos transformar vidas, espalhar amor e esperança, e mostrar ao mundo que a fé pode mover montanhas!',
+                    backgroundImage: '../assets/step2Background.png'
+                },
+                {
+                    title: 'Fica atento a todas as novidades',
+                    text: 'Junta-te à nossa comunidade e recebe as atualizações mais recentes sobre o evento.',
+                    backgroundImage: '../assets/step3Background.png'
+                }
+            ],
+            logo: '../assets/logo_change.png'
+        }
+    },
+    methods: {
+        nextStep() {
+            if (this.step < this.items.length - 1) {
+                this.step++;
+            } else {
+                this.$router.push("/login");
+            }
+        }
+    }
+};
+</script>
+  
+<style scoped>
+.card {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 16px;
+}
+</style>
---
+++ src/pages/Introducao.vue
@@ -0,0 +1,138 @@
+<template>
+    <v-row class="d-flex justify-space-between container-view" no-gutters>
+        <v-col class="d-flex justify-center" cols="12">
+            <v-img lazy-src="../assets/logo_change_white.png" max-height="64" max-width="300" contain
+                src="../assets/logo_change_white.png"></v-img>
+        </v-col>
+        <v-col class="mt-12 px-12" cols="12">
+            <v-row class="d-flex container-options">
+                <div class="option-container">
+                    <v-img lazy-src="../assets/ChooseLanguageIcon.png" max-height="42" max-width="42" contain
+                        src="../assets/ChooseLanguageIcon.png"></v-img>
+                    <p class="text">Choose your language</p>
+                </div>
+                <v-img lazy-src="../assets/ChevronDownIcon.png" max-height="42" max-width="42" contain
+                    src="../assets/ChevronDownIcon.png"></v-img>
+            </v-row>
+            <v-radio-group v-model="language" column class="">
+                <v-row class="option">
+                    <div class="option-container">
+                        <v-img src="../assets/EnglishIcon.png" max-height="42" max-width="42" contain></v-img>
+                        <p class="label-white">English</p>
+                    </div>
+                    <v-radio value="English" class="radio-white" color="white"></v-radio>
+                </v-row>
+                <v-row class="option">
+                    <div class="option-container">
+                        <v-img src="../assets/PortugueseIcon.png" max-height="42" max-width="42" contain></v-img>
+                        <p class="label-white">Portuguese</p>
+                    </div>
+                    <v-radio value="Portuguese" class="radio-white" color="white"></v-radio>
+                </v-row>
+            </v-radio-group>
+        </v-col>
+        <v-col class="mt-2 d-flex justify-center bottom-card" cols="12" no-gutters>
+            <v-btn text class="mt-n1 px-0 btn-next" @click="$router.push('/carrossel')">
+                Próximo
+            </v-btn>
+        </v-col>
+    </v-row>
+</template>
+  
+<script>
+export default {
+    name: 'Introducao',
+    data() {
+        return {
+            language: 'en'
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+  
+<style scoped>
+.option-container {
+    justify-content: start;
+    display: flex;
+    gap: 12px;
+}
+
+.label-white {
+    font-family: 'Inter';
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 23px;
+    letter-spacing: 0px;
+    color: #fff;
+    align-self: center;
+    padding-top: 12px;
+}
+
+.radio-white .v-input__icon--outer {
+    border-color: white;
+}
+
+.radio-white .v-input__icon--inner {
+    background-color: white;
+}
+
+.radio-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.v-radio__label {
+    margin-left: 8px;
+    margin-right: 16px;
+}
+
+.option {
+    justify-content: space-between;
+}
+
+.text {
+    font-family: 'Inter';
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 23px;
+    letter-spacing: 0px;
+    color: #fff;
+    align-self: center;
+    padding-top: 12px;
+}
+
+.container-options {
+    justify-content: space-between;
+    align-items: center;
+}
+
+.btn-next {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 25px;
+    color: #fff;
+    text-transform: none;
+}
+
+.bottom-card {
+    background-color: #2D3540;
+    border-top-left-radius: 48px;
+    border-top-right-radius: 48px;
+    justify-content: center;
+    align-items: center;
+}
+
+.container-view {
+    background-color: #060518;
+    height: 100%;
+    padding-top: 40%
+}
+
+.btn-next {}
+</style>
