9ea1d15f2b146199b36691ce5388ef12d7074212
leonardo compri <leonardocompri46@gmail.com>
2023-06-19 22:03:30 -0300

    Profile

--- src/pages/Conta.vue
+++ src/pages/Conta.vue
@@ -1,58 +1,120 @@
 <template>
-    <v-container fluid class="container">
+    <v-container class="box">
         <v-row>
-            <v-col cols="12" md="4" class="text-center">
+            <v-col cols="12" class="text-center">
                 <v-avatar size="120">
                     <img src="../assets/profile.png" alt="Profile photo">
                 </v-avatar>
                 <div class="mt-2 name">{{ name }}</div>
                 <v-layout class="mt-4 d-flex">
-                    <v-btn style="background-color: #CD2027 !important; font-family: 'Azeret Mono'; font-style: normal; text-transform: none;" class="mr-2 button-red flex-grow-1 flex-shrink-1 white--text" @click="openDonation">
+                    <v-btn
+                        style="background-color: #CD2027 !important; font-family: 'Azeret Mono'; font-style: normal; text-transform: none;"
+                        class="mr-2 button-red flex-grow-1 flex-shrink-1 white--text" @click="openDonation">
                         <v-icon left>mdi-heart</v-icon>
                         Donativo
                     </v-btn>
-                    <v-btn style="background-color: #2F3A4B !important; font-family: 'Azeret Mono'; font-style: normal; text-transform: none;" color="gray dark-3" class="button flex-grow-1 flex-shrink-1 white--text" @click="openTickets">
+                    <v-btn
+                        style="background-color: #2F3A4B !important; font-family: 'Azeret Mono'; font-style: normal; text-transform: none;"
+                        color="gray dark-3" class="button flex-grow-1 flex-shrink-1 white--text" @click="openTickets">
                         <v-icon left>mdi-ticket</v-icon>
                         Bilhetes
                     </v-btn>
                 </v-layout>
             </v-col>
+        </v-row>
+
+        <v-row>
+            <v-col cols="12">
+                <v-card class="card-1 pb-4">
+                    <v-card-title class="azeret title-card-1">
+                        Queres aceitar Jesus como teu senhor e salvador?
+                    </v-card-title>
+
+                    <v-row class="px-4">
+                        <v-col cols="8" v-if="!aceitouJesus">
+                            <v-btn text class="responder-card-1 px-0" @click="dialog = true">
+                                Responder
+                                <v-icon color="#CD2027" size="20" class="mt-1 ml-2">mdi-chevron-right-circle-outline</v-icon>
+                            </v-btn>
+                        </v-col>
+
+                        <v-col v-else>
+                            <p class="azeret sim">
+                                SIM
+                            </p>
+                        </v-col>
+
+                        <v-col cols="4" class="mt-n12">
+                            <v-img max-height="80" max-width="80" src="../assets/praying.gif"></v-img>
+                        </v-col>
+                    </v-row>
+                </v-card>
+            </v-col>
+        </v-row>
+
+        <v-dialog v-model="dialog" persistent max-width="90%">
+            <v-card>
+                <v-card-title class="azeret title-dialog">
+                    Queres aceitar Jesus como teu senhor e salvador?
+                </v-card-title>
+                <v-card-text class="mt-4">
+                    <v-btn class="azeret btn-nao" block x-large @click="dialog = false">
+                        Não
+                    </v-btn>
+                    <v-btn class="azeret btn-sim mt-2" block x-large @click="dialog = false; aceitouJesus = true">
+                        Sim
+                    </v-btn>
+
+                    <v-row>
+                        <v-col class="d-flex justify-center">
+                            <v-img class="mt-2" style="" max-height="80" max-width="80" src="../assets/praying.gif"></v-img>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+
+        <v-row>
+            <v-col cols="12">
+                <v-card class="card-2 pb-4">
+                    <v-card-title class="azeret title-card-2">
+                        Se já Aceitaste Jesus
+                    </v-card-title>
+
+                    <v-card-subtitle class="inter subtitle-card">
+                        Queres re-dedicar a tua vida a Jesus, como o teu senhor e salvador?
+                    </v-card-subtitle>
+
+                    <v-row class="px-4">
+                        <v-col v-if="aceitouJesus">
+                            <p class="azeret sim2">
+                                SIM
+                            </p>
+                        </v-col>
+                        
+                        <v-col v-else cols="offset-8"></v-col>
+                        
+                        <v-col cols="4" class="mt-n12">
+                            <v-img max-height="80" max-width="80" src="../assets/ok.gif"></v-img>
+                        </v-col>
+                    </v-row>
+                </v-card>
+            </v-col>
+        </v-row>
+
+        <v-row class="mb-8">
             <v-col cols="12" md="8">
                 <v-list class="transparent">
-                    <v-list-item @click="selectOption(option1)">
+                    <v-list-item @click="logout()">
                         <v-list-item-avatar style="border-radius: 2px !important;">
                             <v-img src="../assets/close.png"></v-img>
                         </v-list-item-avatar>
-                        <v-list-item-content>
+                        <v-list-item-content class="azeret" style="color:#fff">
                             <v-list-item-title>{{ option1.title }}</v-list-item-title>
                             <v-list-item-subtitle>{{ option1.subtitle }}</v-list-item-subtitle>
                         </v-list-item-content>
                         <v-list-item-action>
-                            <v-icon>{{ option1.icon }}</v-icon>
-                        </v-list-item-action>
-                    </v-list-item>
-                    <v-list-item @click="selectOption(option2)">
-                        <v-list-item-avatar>
-                            <v-img :src="option2.image"></v-img>
-                        </v-list-item-avatar>
-                        <v-list-item-content>
-                            <v-list-item-title>{{ option2.title }}</v-list-item-title>
-                            <v-list-item-subtitle>{{ option2.subtitle }}</v-list-item-subtitle>
-                        </v-list-item-content>
-                        <v-list-item-action>
-                            <v-icon>{{ option2.icon }}</v-icon>
-                        </v-list-item-action>
-                    </v-list-item>
-                    <v-list-item @click="selectOption(option3)">
-                        <v-list-item-avatar>
-                            <v-img :src="option3.image"></v-img>
-                        </v-list-item-avatar>
-                        <v-list-item-content>
-                            <v-list-item-title>{{ option3.title }}</v-list-item-title>
-                            <v-list-item-subtitle>{{ option3.subtitle }}</v-list-item-subtitle>
-                        </v-list-item-content>
-                        <v-list-item-action>
-                            <v-icon>{{ option3.icon }}</v-icon>
+                            <v-icon color="#fff">{{ option1.icon }}</v-icon>
                         </v-list-item-action>
                     </v-list-item>
                 </v-list>
@@ -69,7 +131,7 @@ export default {
             profilePhoto: require("../assets/profile.png"),
             name: "Ricardo Afonso",
             option1: {
-                title: "Editar conta",
+                title: "Terminar sessão",
                 image: require("../assets/edit.png"),
                 icon: "mdi-chevron-right"
             },
@@ -83,7 +145,9 @@ export default {
                 image: require("../assets/close.png"),
                 icon: "mdi-chevron-right"
             },
-            backgroundImage: require("../assets/step1Background.png")
+            backgroundImage: require("../assets/step1Background.png"),
+            dialog: false,
+            aceitouJesus: false
         };
     },
     methods: {
@@ -95,6 +159,11 @@ export default {
         },
         selectOption(item) {
             // implementar ação para selecionar a opção clicada
+        },
+        logout(){
+            sessionStorage.removeItem("usuario")
+            sessionStorage.removeItem("token")
+            this.$router.push('/login')
         }
     },
     mounted() {
@@ -113,9 +182,8 @@ export default {
     background-color: rgba(0, 0, 0, 0.1);
 }
 
-.container {
+.box {
     background: #060518;
-    display: flex;
     height: 100vh;
     background-image: url("../assets/backgroundAccount.png");
     background-size: contain;
@@ -141,4 +209,91 @@ export default {
     background: #2F3A4B;
 }
 
+.azeret {
+    font-family: 'Azeret Mono';
+    font-style: normal;
+}
+
+.inter {
+    font-family: 'Inter';
+    font-style: normal;
+}
+
+.card-1 {
+    background-color: rgba(218, 218, 218, 1);
+    border: 2px solid #2F3A4B;
+    border-radius: 16px;
+}
+
+.card-2 {
+    background-color: #CD2027;
+    border: 2px solid #2F3A4B;
+    border-radius: 16px;
+}
+
+.title-card-1 {
+    word-break: break-word;
+    font-weight: 500;
+    font-size: 21px;
+    line-height: 25px;
+    color: #1C2026;
+}
+
+.title-card-2 {
+    word-break: break-word;
+    font-weight: 500;
+    font-size: 21px;
+    line-height: 25px;
+    color: #fff;
+}
+
+.subtitle-card {
+    word-break: break-word;
+    width: 65%;
+    font-weight: 300;
+    font-size: 16px;
+    line-height: 25px;
+    color: #fff !important;
+}
+
+.responder-card-1 {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 18px;
+    color: #CD2027;
+}
+
+.title-dialog{
+    font-weight: 500;
+    font-size: 21px;
+    line-height: 25px;
+    color: #1C2026;
+    word-break: break-word;
+}
+
+.btn-nao{
+    background: #2F3A4B !important;
+    border-radius: 4px;
+    color: #fff;
+}
+
+.btn-sim{
+    background: #CD2027 !important;
+    border-radius: 4px;
+    color: #fff;
+}
+
+.sim{
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 18px;
+    color: #CD2027;
+}
+
+.sim2{
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 18px;
+    color: #fff;
+}
 </style>
